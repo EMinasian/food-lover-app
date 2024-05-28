f920cab28b5530a9a193c5355091fedbdbc552ea
erik.minasian <erik.minasian@coliquio.de>
2024-05-28 13:41:55 +0400

    feat: main header added

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,9 +1,28 @@
-import logoImage from '@/public/images/logo.png'
+import Link from "next/link";
+import logoImage from "@/assets/logo.png";
+
+const headerNavigationItems = [
+  { link: "/community", text: "Community" },
+  { link: "/meals", text: "Meals" },
+];
 
 export default function MainHeader() {
-    return (
-        <head>
-            <img src={logoImage.src} />
-        </head>
-    )
+  return (
+    <header>
+      <Link href="/">
+        <img src={logoImage.src} />
+        Home Page
+      </Link>
+
+      <nav>
+        <ul>
+          {headerNavigationItems.map((item) => (
+            <li>
+              <Link href={item.link}>{item.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
