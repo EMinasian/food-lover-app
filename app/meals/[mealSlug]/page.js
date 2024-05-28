cdfc8588da25f2c30b1266deb2d4c700fd447997
erik.minasian <erik.minasian@coliquio.de>
2024-05-28 12:57:09 +0400

    feat: routing and navigation

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function DynamicPage() {
+    return (
+      <main>
+        <h1>Dynamic Page</h1>
+      </main>
+    );
+  }
---
+++ components/MainHeader.js
@@ -0,0 +1,9 @@
+import logoImage from '@/public/images/logo.png'
+
+export default function MainHeader() {
+    return (
+        <head>
+            <img src={logoImage.src} />
+        </head>
+    )
+}
