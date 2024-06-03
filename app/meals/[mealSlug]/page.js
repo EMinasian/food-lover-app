3b7aad61ed845a8047f3e12cbecbd7629940a342
erik.minasian <erik.minasian@coliquio.de>
2024-06-03 19:27:51 +0400

    feat: meals details page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,12 @@
-export default function DynamicPage() {
-    return (
-      <main>
-        <h1>Dynamic Page</h1>
-      </main>
-    );
-  }
+import getMeals from "@/utils/getMeals";
+import HeadingComponent from "@/components/HeadingComponent";
+
+export default async function DynamicPage({ params }) {
+  const meal = await getMeals(params?.mealSlug);
+
+  return (
+    <main className="p-16">
+      <HeadingComponent type="primary">{meal?.title}</HeadingComponent>
+    </main>
+  );
+}
--- utils/getMeals.js
+++ utils/getMeals.js
@@ -2,7 +2,15 @@ import sql from "better-sqlite3";
 
 const db = sql("meals.db");
 
-export default async function getMeals() {
+export default async function getMeals(mealSlug) {
   await new Promise((resolve) => setTimeout(resolve, 2 * 1000)); //for learning purposes
+
+  if (mealSlug) {
+    const meals = db.prepare(`SELECT * FROM MEALS WHERE slug = @mealSlug`).all({
+      mealSlug,
+    });
+    return meals[0];
+  }
+
   return db.prepare("SELECT * FROM MEALS").all();
 }
