e6923d8f59c35ee7edec24e2af2a318d8082a22b
erik.minasian <erik.minasian@coliquio.de>
2024-06-05 12:27:55 +0400

    feal: meal item page constructed

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,43 @@
+import Image from "next/image";
 import getMeals from "@/utils/getMeals";
-import HeadingComponent from "@/components/HeadingComponent";
+import HeroBlock from "@/components/HeroBlock";
+
+function handleInstructionItems(instructions) {
+  const instruationsItems = instructions.split("\n\n");
+  console.log("instruationsItems", instruationsItems);
+  return (
+    <ol className="w-2/3 mx-4 text-white flex flex-col gap-4">
+      {instruationsItems.map((item) => (
+        <li>{item}</li>
+      ))}
+    </ol>
+  );
+}
 
 export default async function DynamicPage({ params }) {
   const meal = await getMeals(params?.mealSlug);
 
-  return <HeadingComponent type="primary">{meal?.title}</HeadingComponent>;
+  console.log(meal.instructions);
+
+  return (
+    <>
+      <HeroBlock heading={meal?.title} subHeading={meal.summary}>
+        <span className="text-white">
+          by{" "}
+          <a
+            className="text-orange-500 underline"
+            href={`mailto:${meal.creator_email}`}
+          >
+            {meal.creator}
+          </a>
+        </span>
+      </HeroBlock>
+      <div className="flex">
+        <div className="w-1/4 aspect-square relative mx-4">
+          <Image src={meal.image} fill className="rounded-2xl" />
+        </div>
+        <div>{handleInstructionItems(meal.instructions)}</div>
+      </div>
+    </>
+  );
 }
--- components/HeroBlock.js
+++ components/HeroBlock.js
@@ -2,7 +2,7 @@ import HeadingComponent from "./HeadingComponent";
 
 export default function HeroBlock({ heading, subHeading, children }) {
   return (
-    <div className="flex flex-col p-24 gap-2">
+    <div className="flex flex-col p-16 gap-2">
       <HeadingComponent type="primary">{heading}</HeadingComponent>
       <HeadingComponent type="secondary">{subHeading}</HeadingComponent>
       <div className="flex gap-4 py-2">{children}</div>
