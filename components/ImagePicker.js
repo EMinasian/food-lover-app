030080e2f9874b2cf87c8f1b6c7c35f9161b4f4c
erik.minasian <erik.minasian@coliquio.de>
2024-06-11 18:42:39 +0400

    feat: display for image picker

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,14 +1,30 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function ImagePicker() {
+  const [selectedImage, setSelectedImage] = useState(null);
   const imageInputRef = useRef();
+
+  function imageChangeHandler(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setSelectedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
 
   return (
     <div>
       <button
         type="button"
-        className="bg-amber-500 py-1 px-8 mx-2 font-semibold rounded-md text-sm"
+        className="bg-amber-500 py-1 px-8 font-semibold rounded-md text-sm"
         onClick={() => {
           imageInputRef.current.click();
         }}
@@ -21,8 +37,14 @@ export default function ImagePicker() {
           name="image"
           accept="image/png, image/jpeg"
           ref={imageInputRef}
+          onChange={imageChangeHandler}
         />
       </label>
+      {selectedImage && (
+        <div className="p-8 border-2 border-orange-600 m-4 rounded-lg flex justify-center">
+          <img src={selectedImage} className="w-3/4" />
+        </div>
+      )}
     </div>
   );
 }
