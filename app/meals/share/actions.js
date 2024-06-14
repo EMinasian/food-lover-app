bfb406ad31a2588212ea1ef140efa0b463a9cea7
erik.minasian <erik.minasian@coliquio.de>
2024-06-14 15:50:37 +0400

    feat: server side validation of input

--- app/meals/share/actions.js
+++ app/meals/share/actions.js
@@ -1,6 +1,10 @@
 "use server";
 import saveMeal from "@/utils/saveMeal";
 
+function fieldsInvalid(fields) {
+  return fields?.some((filed) => !filed || filed?.trim() === "");
+}
+
 export default async function shareAction(formData) {
   const meal = {
     creator: formData.get("creator"),
@@ -9,6 +13,10 @@ export default async function shareAction(formData) {
     summary: formData.get("summary"),
     instructions: formData.get("instructions"),
   };
+
+  if (fieldsInvalid(Object.values(meal))) {
+    throw new Error("The fileds in the form are invalid.");
+  }
 
   await saveMeal(meal);
 }
